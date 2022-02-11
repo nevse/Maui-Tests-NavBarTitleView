75d7ee91e553186d497d2245a39e42fa98ab5caf
Eugene Novikov <eugene.novikov@devexpress.com>
2022-02-11 11:50:59 +0300

    first commit

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TitleViewTest;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
